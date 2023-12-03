3cc6690859c4221daa81d70863c8842cac6808ab
Ivan Garkalin <ivan.garkalin2005@mail.ru>
2023-12-03 15:56:17 +0300

    Add read func in func.c

--- func.c
+++ func.c
@@ -14,3 +14,18 @@ void write(struct console *arr, int n){
 
 	fclose(f); 
 }
+
+void read(struct console **arr, int *n){
+	FILE *f = fopen("file.txt", "r");
+	if (!f) return;
+
+	if (fscanf(f, "%d", n) != 1) return;
+	if (*n <= 0) return;
+
+	*arr = (struct console*)calloc(*n, sizeof(struct console));
+	for (int i = 0; i < *n; i++)
+	fscanf(f, "%s %d %d", (arr*)[i].name, &(arr*)[i].price, &(arr*)[i].count);
+
+	fclose(f);
+}
+
