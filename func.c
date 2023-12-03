689d60b81aa2d42c146b056ad5b5a337b5af79cc
Ivan Garkalin <ivan.garkalin2005@mail.ru>
2023-12-03 16:04:50 +0300

    Add create func in func.c

--- func.c
+++ func.c
@@ -24,8 +24,36 @@ void read(struct console **arr, int *n){
 
 	*arr = (struct console*)calloc(*n, sizeof(struct console));
 	for (int i = 0; i < *n; i++)
-	fscanf(f, "%s %d %d", (arr*)[i].name, &(arr*)[i].price, &(arr*)[i].count);
+	fscanf(f, "%s %d %d", (*arr)[i].name, &(*arr)[i].price, &(*arr)[i].count);
 
 	fclose(f);
 }
 
+void create(struct console **arr, int *n){
+	printf("n = ");
+	scanf("%d", n);
+	if(*n <= 0) return;
+
+	*arr = (struct console*)calloc(*n, sizeof(struct console));
+	for (int i = 0; i < *n; i++){
+		printf("name = ");
+		scanf("%s", (*arr)[i].name);
+		printf("price = ");
+		scanf("%d", &(*arr)[i].price);
+
+		while (&(*arr)[i].price <= 0){
+			printf("Incorrect price!\n");
+			prinf("price = ");
+			scanf("%d", &(*arr)[i].price); 
+		}
+
+		printf("count = ");
+		scanf("%d", &(*arr)[i].count);
+
+		while (&(*arr)[i].count < 0){
+			printf("Incorrect count!\n");
+			printf("count = ");
+			scanf("%d", &(*arr)[i].count);
+		}
+	}
+}
