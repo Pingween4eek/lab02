f50eba1421669d1088f83d7c79f6f3d4a1a9e583
Ivan Garkalin <ivan.garkalin2005@mail.ru>
2023-12-03 15:44:39 +0300

    Add func.h file

---
+++ func.h
@@ -0,0 +1,9 @@
+struct console{
+	char name[50];
+	int price;
+	int count;
+}
+
+void write(struct console *arr, int n);
+void read(struct console **arr, int *n);
+void create(struct console **arr, int *n);
